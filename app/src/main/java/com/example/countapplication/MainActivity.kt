4e0c2a81a45eddd144dbc148e844238b525e7fd3
tkr-saitou <yuk-saitou@technocrea.co.jp>
2020-12-15 15:44:26 +0900

    Initial commit

---
+++ app/src/main/java/com/example/countapplication/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.countapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+//import android.util.Log
+import android.widget.Button
+import android.widget.TextView
+
+
+class MainActivity : AppCompatActivity() {
+    private var count: Int = 0
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        //Buttonを取得
+        val button = findViewById<Button>(R.id.count_button)
+        //TextViewを取得
+        val textView = findViewById<TextView>(R.id.count_text)
+        //ボタンイベントを通知
+        button.setOnClickListener {
+            count += 1
+            textView.text = count.toString()
+        //Log.d("MainActivity","Button Click!")
+        }
+    }
+}
